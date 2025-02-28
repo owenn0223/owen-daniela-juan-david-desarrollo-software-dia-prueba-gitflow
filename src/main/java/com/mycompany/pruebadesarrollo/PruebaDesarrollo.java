d6ce6cbfa80ea57e7b393ae624a55f0c698ec31d
owen <bareno.owen.9508@eam.edu.co>
2025-02-28 11:05:34 -0500

    primer commit de bienvenida

---
+++ src/main/java/com/mycompany/pruebadesarrollo/PruebaDesarrollo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebadesarrollo;
+
+/**
+ *
+ * @author PC
+ */
+public class PruebaDesarrollo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
